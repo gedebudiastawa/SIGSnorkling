a5f33b4dbcbb78b76d61019b7bf26a16c0418826
unknown <astawagedebudi@gmail.com>
2020-06-10 01:46:20 +0800

    first commit

---
+++ app/src/main/java/com/example/gissnorkling/DetailActivity.java
@@ -0,0 +1,49 @@
+package com.example.gissnorkling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView datanama, dataalamat, datatelepon;
+    Button btncall;
+    ImageView datagambar;
+    public static String id, nama, alamat, gambar, telepon;
+    public static Double latitude, longitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        datanama = findViewById(R.id.tv_nama);
+        dataalamat = findViewById(R.id.tv_alamat);
+        datatelepon = findViewById(R.id.tv_telepon);
+        btncall = findViewById(R.id.btn_call);
+        datagambar = findViewById(R.id.img_data);
+
+        datanama.setText(nama);
+        dataalamat.setText(alamat);
+        datatelepon.setText(telepon);
+        Picasso.get().load(gambar).into(datagambar);
+
+        btncall.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent call =new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+telepon));
+        startActivity(call);
+
+
+    }
+}
